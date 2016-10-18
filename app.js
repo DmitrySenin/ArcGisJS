bf25bd0ce9f9ad960bebcbf5f9f34f7f5b3cb618
dmitry.senin <white.lord.mane@gmail.com>
2016-10-18 18:32:22 +0300

    Change selector and js module

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 ;(function() {
-	require([
-      "esri/Map",
-      "esri/views/MapView",
-      "dojo/domReady!"
-    ], function(Map, MapView) {
+	require(['esri/Map', 'esri/views/MapView', 'dojo/domReady!'], MapBuilder);
 
-      var map = new Map({
-        basemap: "streets"
-      });
+  function MapBuilder(Map, MapView) {
 
-      var view = new MapView({
-        container: "viewDiv",
-        map: map,
-        zoom: 4,
-        center: [15, 65]
-      });
-
+    var map = new Map({
+      basemap: "streets"
     });
+
+    var view = new MapView({
+      container: 'mapContainer',
+      map: map,
+      zoom: 4,
+      center: [15, 65]
+    });
+
+  }
 })();
